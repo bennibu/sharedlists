17e6519067dafeaebda678efd72f77c13241b5a8
Benjamin Meichsner <benjamin.meichsner@aboutsource.net>
2013-07-02 19:24:09 +0200

    Schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,17 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121017084147) do
+ActiveRecord::Schema.define(:version => 20130627093146) do
 
   create_table "articles", :force => true do |t|
     t.string   "name",                                                          :null => false
@@ -27,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20121017084147) do
     t.decimal  "scale_price",    :precision => 8, :scale => 2
     t.datetime "created_on"
     t.datetime "updated_on"
+    t.string   "category"
   end
 
   add_index "articles", ["name"], :name => "index_articles_on_name"
