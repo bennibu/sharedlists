aa7ea022169b6114fb1e8bab3a1594e4c225859c
wvengen <dev-voko@willem.engen.nl>
2013-10-23 12:31:50 +0200

    BD-Totaal parsing improvements

--- lib/bdtotaal_file.rb
+++ lib/bdtotaal_file.rb
@@ -10,7 +10,7 @@ module BdtotaalFile
 
   def self.detect(data)
     # when there is a line starting with BD-Totaal
-    data[0..200].match(/\n\s*(,\s*)*BD-Totaal/m) ? 0.9 : 0
+    data[0..200].match(/(^|\n)\s*BD-Totaal/m) ? 0.9 : 0
   end
   
   # the parsed article is a simple hash
@@ -20,10 +20,12 @@ module BdtotaalFile
     CSV.parse(data, {:col_sep => FileHelper.csv_guess_col_sep(data), :headers => true, :header_converters => headclean}) do |row|
       # skip empty lines
       row[0].blank? and next
+
       # create a new article
       error = nil
       name = row['Artikelomschrijving']
       manuf = row['Merknaam']
+      # some manufacturer names are actually extra product info
       if not manuf.blank? and manuf.match /(verpakt|los)/
         name += " (#{manuf})"
         manuf = nil
@@ -31,7 +33,7 @@ module BdtotaalFile
       unit_price = parse_price(row['Eenheidsprijs'])
       pack_price = parse_price(row['Colloprijs'])
       begin
-        unit, unit_quantity = parse_inhoud(row['Inhoud'], unit_price, pack_price)
+        unit, unit_quantity, unit_price = parse_inhoud(row['Inhoud'], unit_price, pack_price)
       rescue Exception => e
 	unit, unit_quantity = row['Inhoud'], 1
         error = e.message
@@ -60,19 +62,30 @@ module BdtotaalFile
 
   # there is one field containing both unit and unit quantity
   #   "per kg",   "1x300 gr",    "1x1,5 kg",    "1x8x25 gr",    "1x10 bs"
-  # returns unit, unit_quantity
-  #   [1, "kg"],  [1, "300 gr"], [1, "1.5 kg"], [1, "8x25 gr"], [10, "bs"]
+  # returns unit, unit_quantity, unit_price
+  #   [1, "kg",],  [1, "300 gr",], [1, "1.5 kg",], [1, "8x25 gr",], [10, "bs",]
   # sometimes the order is unclear, so we compare the price of one unit with
   # the price of the full pack
   def self.parse_inhoud(s, unit_price, total_price)
-    s.gsub! /^per\s*/, '' and return s, 1
+    s.gsub! /^per\s*/, '' and return s, 1, unit_price
     s.gsub! /,/, '.' # use decimal point
+    # if prices are equal it's easy
+    (unit_price - total_price) < 1e-3 and return s.gsub(/^1x/,''), 1, unit_price
+    # find which of the units is the unit quantity
     parts, unit = s.split /\s+/, 2
     parts = parts.split('x')
     i = parts.index { |p| (p.to_f * unit_price - total_price) < 1e-3 }
-    raise Exception, "Could not find unit quantity for 'Inhoud': #{s}" unless i
+    # if that fails, perhaps the unit_price is the kg-price and total_price is the unit price
+    if i.nil? and parts.length == 2 and parts[0].to_f == 1
+      if unit == 'gr' and (total_price*parts.last.to_f/1000 - unit_price) < 1e-2
+        i = 0; unit_price = total_price
+      elsif unit == 'kg' and (total_price*parts.last.to_f - unit_price) < 1e-2
+        i = 0; unit_price = total_price
+      end
+    end
+    raise Exception, "Could not find unit quantity for 'Inhoud': #{s} (single #{unit_price}, pack #{total_price})" if i.nil?
     unit_quantity = parts.delete_at(i)
-    return "#{parts.join('x')} #{unit}", unit_quantity
+    return "#{parts.join('x')} #{unit}", unit_quantity, unit_price
   end
   
 end
